25a7a5a7d8c47f6fbc508dc87ec6cd5fc82be473
Leon <37100346+Leon-pan@users.noreply.github.com>
2022-05-26 17:30:39 +0800

    Update custom_shell_file.sh

--- custom_shell_file.sh
+++ custom_shell_file.sh
@@ -2,4 +2,5 @@ sed -i '/jd_beauty.js/c\47 9,12,14,15,20,21 * * * node /scripts/jd_beauty.js >> 
 #sed -i '/jd_club_lottery.js/c\6 0,23 * * * node /scripts/jd_club_lottery.js >> /scripts/logs/jd_club_lottery.log 2>&1' /scripts/docker/merged_list_file.sh
 #sed -i '/jd_blueCoin.js/c\59 23 * * * node /scripts/jd_blueCoin.js >> /scripts/logs/jd_blueCoin.log 2>&1' /scripts/docker/merged_list_file.sh
 sed -i '/jd_dreamFactory/'d /scripts/docker/merged_list_file.sh
+sed -i 's/raw.githubusercontent.com/ghspeed.hiddendragon.space\/https:\/\/raw.githubusercontent.com/' /scripts/jd_bean_sign.js
 #sed -i '/rm -rf/c\50 23 */3 * * rm -rf /scripts/logs/*.log' /scripts/docker/merged_list_file.sh
